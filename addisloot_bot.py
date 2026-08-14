dd5c61ba22d6ec2393ea72128e2d7c0407a4b673
NARD304 <narobikadereje749@gmail.com>
2026-08-14 18:43:46 +0300

    Update addisloot_bot.py

--- addisloot_bot.py
+++ addisloot_bot.py
@@ -38,7 +38,7 @@ MERCHANT_PAY_TO = "0983762777"
 
 FT_API_ID = "RS8XTD55WTQ0RU7V"
 
-FT_API_KEY = "" # <-- paste your real FlashTopup API KEY here
+FT_API_KEY = "" #88a2bdcd1e2973cd06b858aa430fda063e49bc4d54bb91ae6627233a4406673a
 
 FT_BASE_URL = "https://api.flashtopup.com/api/reseller/v2"
 
